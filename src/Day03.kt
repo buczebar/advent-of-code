43d1ee659d84213a178b915b1a51b1013682492e
buczebar <bartosz.buczek@protonmail.com>
2022-12-03 19:16:51 +0100

    Day03 small refactor

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,19 +1,16 @@
 fun main() {
-
-    fun getPriority(item: Char) =
-        (('a'..'z') + ('A'..'Z')).indexOf(item) + 1
+    fun Char.getPriority() =
+        (('a'..'z') + ('A'..'Z')).indexOf(this) + 1
 
     fun part1(input: List<String>) =
         input.map { sack -> sack.chunked(sack.length / 2).map { comp -> comp.toList() } }
-            .map { (comp1, comp2) -> comp1.intersect(comp2.toSet()) }
-            .flatten()
-            .sumOf { getPriority(it) }
+            .map { (comp1, comp2) -> (comp1 intersect comp2.toSet()).single() }
+            .sumOf { it.getPriority() }
 
     fun part2(input: List<String>) =
         input.chunked(3)
-            .map { (a, b, c) -> a.toSet().intersect(b.toSet().intersect(c.toSet())) }
-            .flatten()
-            .sumOf { getPriority(it) }
+            .map { (a, b, c) -> (a.toSet() intersect b.toSet() intersect c.toSet()).single() }
+            .sumOf { it.getPriority() }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
