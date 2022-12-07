e54d932368ee8b16a956888f5592fc6cffe68975
buczebar <bartosz.buczek@protonmail.com>
2022-12-07 15:01:37 +0100

    Day07 No Space Left On Device

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ using [Kotlin](https://kotlinlang.org) language.
 | 04  | [Camp Cleanup](src/Day04.kt)            |   🌟   |   🌟   |
 | 05  | [Supply Stacks](src/Day05.kt)           |   🌟   |   🌟   |
 | 06  | [Tuning Trouble](src/Day06.kt)          |   🌟   |   🌟   |
+| 07  | [No Space Left On Device](src/Day07.kt) |   🌟   |   🌟   |
 
 [^aoc]: [Advent of Code](https://adventofcode.com) – an annual event in December since 2015.
 Every year since then, with the first day of December, a programming puzzles contest is published every day for
---
+++ src/Day07.kt
@@ -0,0 +1,84 @@
+private const val TOTAL_DISK_SPACE = 70000000L
+private const val SPACE_NEEDED_FOR_UPDATE = 30000000L
+fun main() {
+    fun part1(fileTree: TreeNode): Long {
+        return fileTree.getAllSubdirectories().filter { it.totalSize <= 100_000 }.sumOf { it.totalSize }
+    }
+
+    fun part2(fileTree: TreeNode): Long {
+        val spaceAvailable = TOTAL_DISK_SPACE - fileTree.totalSize
+        val spaceToBeFreed = SPACE_NEEDED_FOR_UPDATE - spaceAvailable
+        return fileTree.getAllSubdirectories().sortedBy { it.totalSize }
+            .firstOrNull { it.totalSize > spaceToBeFreed }?.totalSize ?: 0L
+    }
+
+    val testInput = readInput("Day07_test")
+    val testInputFileTree = buildFileTree(testInput)
+    check(part1(testInputFileTree) == 95_437L)
+    check(part2(testInputFileTree) == 24_933_642L)
+
+    val input = readInput("Day07")
+    val inputFileTree = buildFileTree(input)
+    println(part1(inputFileTree))
+    println(part2(inputFileTree))
+}
+
+private fun buildFileTree(consoleLog: List<String>): TreeNode {
+    val rootNode = DirNode("/", null)
+    var currentNode = rootNode as TreeNode
+    for (line in consoleLog) {
+        if ("\\$ cd [\\w/]+".toRegex().matches(line)) {
+            val name = "\\$ cd ([\\w/]+)".toRegex().find(line)!!.groups[1]?.value ?: "/"
+            currentNode = currentNode.getChildByName(name)
+        } else if (line.startsWith("dir")) {
+            val (_, name) = "(\\w+) (\\w+)".toRegex().find(line)!!.destructured
+            currentNode.addChild(DirNode(name))
+        } else if ("\\d+ \\w+.*\\w*".toRegex().matches(line)) {
+            val matches = "(\\d+) (\\w+.*\\w*)".toRegex().find(line)?.groups
+            if (matches?.size == 3) {
+                currentNode.addChild(FileNode(matches[2]?.value!!, matches[1]?.value!!.toLong()))
+            }
+        } else if (line == "$ cd ..") {
+            currentNode = currentNode.parent ?: break
+        }
+    }
+    return rootNode
+}
+
+private abstract class TreeNode(private val name: String, var parent: TreeNode?, val children: MutableList<TreeNode>?) {
+    open val totalSize: Long
+        get() {
+            return children?.sumOf { it.totalSize } ?: 0
+        }
+
+    fun addChild(node: TreeNode) {
+        node.parent = this
+        children?.add(node)
+    }
+
+    fun getChildByName(name: String): TreeNode {
+        return children?.firstOrNull { it.name == name } ?: this
+    }
+
+    fun getAllSubdirectories(): List<DirNode> {
+        return getAllSubNodes().filterIsInstance<DirNode>()
+    }
+
+    private fun getAllSubNodes(): List<TreeNode> {
+        val result = mutableListOf<TreeNode>()
+        if (children != null) {
+            for (child in children) {
+                result.addAll(child.getAllSubNodes())
+                result.add(child)
+            }
+        }
+        return result
+    }
+}
+
+private class DirNode(name: String, parent: TreeNode? = null, children: MutableList<TreeNode> = mutableListOf()) :
+    TreeNode(name, parent, children)
+
+private class FileNode(name: String, override val totalSize: Long, parent: DirNode? = null) :
+    TreeNode(name, parent, null)
+
